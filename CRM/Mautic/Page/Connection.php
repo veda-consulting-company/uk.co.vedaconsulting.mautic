aa532456974fe40685067069cf15c0665d8f4e7f
Matthew Wire <mjw@mjwconsult.co.uk>
2020-12-24 13:43:57 +0000

    Fix mautic version on connection page

--- CRM/Mautic/Page/Connection.php
+++ CRM/Mautic/Page/Connection.php
@@ -317,7 +317,7 @@ EOF;
       // Retrieve the connected user as a test.
       $this->apiUser = $testApi->getSelf();
       $responseInfo = $auth->getResponseInfo();
-      $this->mauticVersion = $testApi->getMauticVersion();
+      $this->mauticVersion = $testApi->getResponseHeaders()['mautic-version'] ?? 'Unknown';
       return 200 == $responseInfo['http_code'];
     }
     return FALSE;
