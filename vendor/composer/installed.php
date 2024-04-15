c73f09e8e998eec6391ea12c65d1783065c733f3
Che Molava <che@vedaconsulting.co.uk>
2024-04-15 17:20:24 +0000

    Update Mautic API Library from 3.0.0 to  3.1.0.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,38 @@
+<?php return array(
+    'root' => array(
+        'name' => 'uk.co.vedaconsulting/mautic',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '70858fb66c36087af56534b7a581a80a2f071f97',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'mautic/api-library' => array(
+            'pretty_version' => '3.1.0',
+            'version' => '3.1.0.0',
+            'reference' => '5b0d9051a3a42eea132fa6da35b712e947804d2c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../mautic/api-library',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'psr/log' => array(
+            'dev_requirement' => false,
+            'replaced' => array(
+                0 => '*',
+            ),
+        ),
+        'uk.co.vedaconsulting/mautic' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '70858fb66c36087af56534b7a581a80a2f071f97',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
