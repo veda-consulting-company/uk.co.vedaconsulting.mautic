eae0ae1ae4b18dcbee40649ccbc34de4d9ec5cd1
colemanw <coleman@civicrm.org>
2025-10-22 21:06:49 -0400

    Replace reference to deprecated exception class
    The api-specific exception classes have been deprecated and will soon be removed from core.
    They are identical to CRM_Core_Exception.

--- api/v3/Mautic/Pushsync.php
+++ api/v3/Mautic/Pushsync.php
@@ -19,7 +19,7 @@ function _civicrm_api3_mautic_Pushsync_spec(&$spec) {
  * @return array API result descriptor
  * @see civicrm_api3_create_success
  * @see civicrm_api3_create_error
- * @throws API_Exception
+ * @throws CRM_Core_Exception
  */
 function civicrm_api3_mautic_Pushsync($params) {
   // Do push from CiviCRM
